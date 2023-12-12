e4500f2ce917ceb46372f8025a85b0ca73474b95
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2023-12-12 17:06:13 +0530

    Add files via upload

---
+++ EmployeeManagementSystemApplication2.java
@@ -0,0 +1,13 @@
+package com.dev_amit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemApplication2.class, args);
+	}
+
+}
